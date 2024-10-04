2929dacf26e8639b78faf3a51210848a2ddba3be
BRENO OLIVEIRA GOMES <BRENO6162@DIGITALCOLLEGE.LOCAL>
2024-10-03 21:37:03 -0300

    usando useStates

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [nome, setNome] = useState('Breno')
+  let novoNome = 'Gustavo';
+  const alunos = [
+    {
+      nome: 'Alberto',
+      nomeCompleto: 'Alberto Wowtilla Silva Galdino'
+    },
+    {
+      nome: 'Breno',
+      nomeCompleto: 'Breno Oliveira Gomes'
+    },
+    {
+      nome: 'Gustavo',
+      nomeCompleto: 'Luis Gustavo'
+    }
+  ]
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {alunos.map((alunos) =>
+        <>
+        <h3>{alunos.nome}</h3>
+        <p>{alunos.nomeCompleto}</p>
+        </>
+        
+      )}
+
     </>
   )
 }
